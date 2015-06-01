996a69299a19d2f26fec6ca83db7a481312e97a5
kunwardeep <kunwardeep.gill@gmail.com>
2015-06-01 10:37:40 -0400

    VC modifications

--- ParseSwiftStarterProject/ParseStarterProject/ViewController.swift
+++ ParseSwiftStarterProject/ParseStarterProject/ViewController.swift
@@ -9,6 +9,21 @@ import Parse
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var userNameTextField: UITextField!
+  @IBAction func submitButtonPressed(sender: AnyObject) {
+    //  Login User
+    PFUser.logInWithUsernameInBackground(self.userNameTextField.text, password: "myPass") { (user:PFUser?, error:NSError?) -> Void in
+      
+      if user != nil {
+        println("The user is logged in")
+      }
+      else
+      {
+        println("Login Failed")
+      }
+      
+    }
+  }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
